3db72806bf89ce8475a8e2fb9cc32738d8858df9
pong3ds <chaiyapong@3dsinteractive.com>
2021-09-12 18:04:47 +0700

    Finish chapter 3

---
+++ chapter-03-optimize-set-get-with-mset-mget/3.1-optimize-set-get-to-mset-mget/main.go
@@ -0,0 +1,239 @@
+package main
+
+import (
+	"net/http"
+
+	_ "github.com/3dsinteractive/wrkgo"
+)
+
+func main() {
+
+	cfg := NewConfig()
+
+	// 1. Create microservices
+	ms := NewMicroservice()
+
+	// 2. Migrate and seed 100,000 members
+	ms.Log("Main", "Migrate database...")
+	err := setup(cfg)
+	if err != nil {
+		ms.Log("Main", err.Error())
+		return
+	}
+
+	// 3. GET api query direct from database
+	ms.GET("/api", func(ctx IContext) error {
+		// Query 1
+		members, err := queryLastestMembersFromDatabase(ctx, cfg)
+		if err != nil {
+			ctx.Response(http.StatusInternalServerError, map[string]interface{}{"status": "error"})
+			return nil
+		}
+		// Query 2
+		counter, err := queryCountAllMembersFromDatabase(ctx, cfg)
+		if err != nil {
+			ctx.Response(http.StatusInternalServerError, map[string]interface{}{"status": "error"})
+			return nil
+		}
+
+		resp := map[string]interface{}{
+			"status": "ok",
+			"total":  counter,
+			"items":  members,
+		}
+		ctx.Response(http.StatusOK, resp)
+		return nil
+	})
+
+	// 4. GET api using cache at data layer
+	//    the benefit of data layer cache, is data can be shared at other api
+	// ms.GET("/api", func(ctx IContext) error {
+
+	// 	query1CacheKey := "members::latest"
+	// 	members := []*Member{}
+
+	// 	cacheTimeout := 60 * 2 * time.Second
+	// 	cacher := ctx.Cacher(NewCacherConfig())
+	// 	membersJS, err := cacher.Get(query1CacheKey)
+	// 	if err != nil {
+	// 		ctx.Log(err.Error())
+	// 	}
+
+	// 	// Found query #1 cache
+	// 	if len(membersJS) > 0 {
+	// 		// ctx.Log("cache hit")
+	// 		err := json.Unmarshal([]byte(membersJS), &members)
+	// 		if err != nil {
+	// 			cacher.Del(query1CacheKey)
+	// 			ctx.Log(err.Error())
+	// 		}
+	// 	}
+
+	// 	if len(membersJS) == 0 {
+	// 		// ctx.Log("cache miss")
+	// 		members, err = queryLastestMembersFromDatabase(ctx, cfg)
+	// 		if err != nil {
+	// 			ctx.Response(http.StatusInternalServerError, map[string]interface{}{"status": "error"})
+	// 			return nil
+	// 		}
+
+	// 		err = cacher.Set(query1CacheKey, members, cacheTimeout)
+	// 		if err != nil {
+	// 			ctx.Log(err.Error())
+	// 		}
+	// 	}
+
+	// 	query2CacheKey := "members::total"
+	// 	counter := -1
+	// 	counterJS, err := cacher.Get(query2CacheKey)
+	// 	if err != nil {
+	// 		ctx.Log(err.Error())
+	// 	}
+
+	// 	// Found query #2 cache
+	// 	if len(counterJS) > 0 {
+	// 		// ctx.Log("cache hit")
+	// 		counter, err = strconv.Atoi(counterJS)
+	// 		if err != nil {
+	// 			counter = -1
+	// 			cacher.Del(query2CacheKey)
+	// 			ctx.Log(err.Error())
+	// 		}
+	// 	}
+
+	// 	if counter < 0 {
+	// 		// ctx.Log("cache miss")
+	// 		counter, err = queryCountAllMembersFromDatabase(ctx, cfg)
+	// 		if err != nil {
+	// 			ctx.Response(http.StatusInternalServerError, map[string]interface{}{"status": "error"})
+	// 			return nil
+	// 		}
+	// 		err = cacher.SetS(query2CacheKey, fmt.Sprintf("%d", counter), cacheTimeout)
+	// 		if err != nil {
+	// 			ctx.Log(err.Error())
+	// 		}
+	// 	}
+
+	// 	resp := map[string]interface{}{
+	// 		"status": "ok",
+	// 		"total":  counter,
+	// 		"items":  members,
+	// 	}
+	// 	ctx.Response(http.StatusOK, resp)
+	// 	return nil
+	// })
+
+	// 5. GET api using cache at data layer
+	//    usnig MSET and MGET to optimize
+	// ms.GET("/api", func(ctx IContext) error {
+
+	// 	query1CacheKey := "members::latest"
+	// 	query2CacheKey := "members::total"
+
+	// 	members := []*Member{}
+	// 	counter := -1
+
+	// 	cacher := ctx.Cacher(NewCacherConfig())
+	// 	cacheItems, err := cacher.MGet([]string{query1CacheKey, query2CacheKey})
+	// 	if err != nil {
+	// 		ctx.Log(err.Error())
+	// 	}
+
+	// 	// The len of cacheItems will equal to the keys we send when call MGet
+	// 	membersJS := cacheItems[0]
+	// 	counterJS := cacheItems[1]
+
+	// 	// Found query #1 cache
+	// 	if membersJS != nil && len(membersJS.(string)) > 0 {
+	// 		// ctx.Log("cache hit")
+	// 		err := json.Unmarshal([]byte(membersJS.(string)), &members)
+	// 		if err != nil {
+	// 			cacher.Del(query1CacheKey)
+	// 			ctx.Log(err.Error())
+	// 		}
+	// 	}
+
+	// 	itemToCaches := map[string]interface{}{}
+
+	// 	if membersJS == nil {
+	// 		// ctx.Log("cache miss")
+	// 		members, err = queryLastestMembersFromDatabase(ctx, cfg)
+	// 		if err != nil {
+	// 			ctx.Response(http.StatusInternalServerError, map[string]interface{}{"status": "error"})
+	// 			return nil
+	// 		}
+
+	// 		itemToCaches[query1CacheKey] = members
+	// 	}
+
+	// 	// Found query #2 cache
+	// 	if counterJS != nil && len(counterJS.(string)) > 0 {
+	// 		// ctx.Log("cache hit")
+	// 		counter, err = strconv.Atoi(counterJS.(string))
+	// 		if err != nil {
+	// 			counter = -1
+	// 			cacher.Del(query2CacheKey)
+	// 			ctx.Log(err.Error())
+	// 		}
+	// 	}
+
+	// 	if counter < 0 {
+	// 		// ctx.Log("cache miss")
+	// 		counter, err = queryCountAllMembersFromDatabase(ctx, cfg)
+	// 		if err != nil {
+	// 			ctx.Response(http.StatusInternalServerError, map[string]interface{}{"status": "error"})
+	// 			return nil
+	// 		}
+
+	// 		itemToCaches[query2CacheKey] = fmt.Sprintf("%d", counter)
+	// 	}
+
+	// 	if len(itemToCaches) > 0 {
+	// 		err = cacher.MSet(itemToCaches)
+	// 		if err != nil {
+	// 			ctx.Log(err.Error())
+	// 		}
+	// 	}
+
+	// 	resp := map[string]interface{}{
+	// 		"status": "ok",
+	// 		"total":  counter,
+	// 		"items":  members,
+	// 	}
+	// 	ctx.Response(http.StatusOK, resp)
+	// 	return nil
+	// })
+
+	// 5. Cleanup when exit
+	defer ms.Cleanup()
+	ms.Start()
+}
+
+func queryLastestMembersFromDatabase(ctx IContext, cfg IConfig) ([]*Member, error) {
+	pst := ctx.Persister(cfg.PersisterConfig())
+	members := make([]*Member, 0)
+	_, err := pst.WhereSP(
+		&members,
+		"register_order desc",
+		30, // limits
+		1,  // pages
+		"is_active = ?",
+		"1")
+	if err != nil {
+		return nil, err
+	}
+	return members, nil
+}
+
+func queryCountAllMembersFromDatabase(ctx IContext, cfg IConfig) (int, error) {
+	pst := ctx.Persister(cfg.PersisterConfig())
+	member := &Member{}
+	counter, err := pst.Count(
+		member,
+		"is_active = ?",
+		"1")
+	if err != nil {
+		return 0, err
+	}
+	return int(counter), nil
+}
