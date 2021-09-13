d0dcd8c97d586024ddb2c18be74b6b0c8f58da89
pong3ds <chaiyapong@3dsinteractive.com>
2021-09-13 10:06:53 +0700

    Use first method first

--- chapter-03-optimize-set-get-with-mset-mget/3.1-optimize-set-get-to-mset-mget/main.go
+++ chapter-03-optimize-set-get-with-mset-mget/3.1-optimize-set-get-to-mset-mget/main.go
@@ -51,81 +51,81 @@ func main() {
 
 	// 4. GET api using cache at data layer
 	//    the benefit of data layer cache, is data can be shared at other api
-	// ms.GET("/api", func(ctx IContext) error {
+	ms.GET("/api", func(ctx IContext) error {
 
-	// 	query1CacheKey := "members::latest"
-	// 	members := []*Member{}
+		query1CacheKey := "members::latest"
+		members := []*Member{}
 
-	// 	timeToExpire := 60 * 5 * time.Second // 5m
-	// 	cacher := ctx.Cacher(NewCacherConfig())
-	// 	membersJS, err := cacher.Get(query1CacheKey)
-	// 	if err != nil {
-	// 		ctx.Log(err.Error())
-	// 	}
+		timeToExpire := 60 * 5 * time.Second // 5m
+		cacher := ctx.Cacher(NewCacherConfig())
+		membersJS, err := cacher.Get(query1CacheKey)
+		if err != nil {
+			ctx.Log(err.Error())
+		}
 
-	// 	// Found query #1 cache
-	// 	if len(membersJS) > 0 {
-	// 		// ctx.Log("cache hit")
-	// 		err := json.Unmarshal([]byte(membersJS), &members)
-	// 		if err != nil {
-	// 			cacher.Del(query1CacheKey)
-	// 			ctx.Log(err.Error())
-	// 		}
-	// 	}
+		// Found query #1 cache
+		if len(membersJS) > 0 {
+			// ctx.Log("cache hit")
+			err := json.Unmarshal([]byte(membersJS), &members)
+			if err != nil {
+				cacher.Del(query1CacheKey)
+				ctx.Log(err.Error())
+			}
+		}
 
-	// 	if len(membersJS) == 0 {
-	// 		// ctx.Log("cache miss")
-	// 		members, err = queryLastestMembersFromDatabase(ctx, cfg)
-	// 		if err != nil {
-	// 			ctx.Response(http.StatusInternalServerError, map[string]interface{}{"status": "error"})
-	// 			return nil
-	// 		}
+		if len(membersJS) == 0 {
+			// ctx.Log("cache miss")
+			members, err = queryLastestMembersFromDatabase(ctx, cfg)
+			if err != nil {
+				ctx.Response(http.StatusInternalServerError, map[string]interface{}{"status": "error"})
+				return nil
+			}
 
-	// 		err = cacher.Set(query1CacheKey, members, timeToExpire)
-	// 		if err != nil {
-	// 			ctx.Log(err.Error())
-	// 		}
-	// 	}
+			err = cacher.Set(query1CacheKey, members, timeToExpire)
+			if err != nil {
+				ctx.Log(err.Error())
+			}
+		}
 
-	// 	query2CacheKey := "members::total"
-	// 	counter := -1
-	// 	counterJS, err := cacher.Get(query2CacheKey)
-	// 	if err != nil {
-	// 		ctx.Log(err.Error())
-	// 	}
+		query2CacheKey := "members::total"
+		counter := -1
+		counterJS, err := cacher.Get(query2CacheKey)
+		if err != nil {
+			ctx.Log(err.Error())
+		}
 
-	// 	// Found query #2 cache
-	// 	if len(counterJS) > 0 {
-	// 		// ctx.Log("cache hit")
-	// 		counter, err = strconv.Atoi(counterJS)
-	// 		if err != nil {
-	// 			counter = -1
-	// 			cacher.Del(query2CacheKey)
-	// 			ctx.Log(err.Error())
-	// 		}
-	// 	}
+		// Found query #2 cache
+		if len(counterJS) > 0 {
+			// ctx.Log("cache hit")
+			counter, err = strconv.Atoi(counterJS)
+			if err != nil {
+				counter = -1
+				cacher.Del(query2CacheKey)
+				ctx.Log(err.Error())
+			}
+		}
 
-	// 	if counter < 0 {
-	// 		// ctx.Log("cache miss")
-	// 		counter, err = queryCountAllMembersFromDatabase(ctx, cfg)
-	// 		if err != nil {
-	// 			ctx.Response(http.StatusInternalServerError, map[string]interface{}{"status": "error"})
-	// 			return nil
-	// 		}
-	// 		err = cacher.SetS(query2CacheKey, fmt.Sprintf("%d", counter), timeToExpire)
-	// 		if err != nil {
-	// 			ctx.Log(err.Error())
-	// 		}
-	// 	}
+		if counter < 0 {
+			// ctx.Log("cache miss")
+			counter, err = queryCountAllMembersFromDatabase(ctx, cfg)
+			if err != nil {
+				ctx.Response(http.StatusInternalServerError, map[string]interface{}{"status": "error"})
+				return nil
+			}
+			err = cacher.SetS(query2CacheKey, fmt.Sprintf("%d", counter), timeToExpire)
+			if err != nil {
+				ctx.Log(err.Error())
+			}
+		}
 
-	// 	resp := map[string]interface{}{
-	// 		"status": "ok",
-	// 		"total":  counter,
-	// 		"items":  members,
-	// 	}
-	// 	ctx.Response(http.StatusOK, resp)
-	// 	return nil
-	// })
+		resp := map[string]interface{}{
+			"status": "ok",
+			"total":  counter,
+			"items":  members,
+		}
+		ctx.Response(http.StatusOK, resp)
+		return nil
+	})
 
 	// 5. GET api using cache at data layer
 	//    using MSET and MGET to optimize
@@ -222,125 +222,125 @@ func main() {
 	// })
 
 	// 6. GET api using cache at data layer and local memcache
-	ms.GET("/api", func(ctx IContext) error {
+	// ms.GET("/api", func(ctx IContext) error {
 
-		query1CacheKey := "members::latest"
-		query2CacheKey := "members::total"
+	// 	query1CacheKey := "members::latest"
+	// 	query2CacheKey := "members::total"
 
-		members := []*Member{}
-		counter := -1
+	// 	members := []*Member{}
+	// 	counter := -1
 
-		// 1. Find from memory first, if found, then return
-		keys := []string{query1CacheKey, query2CacheKey}
-		memcacher := ctx.MemCacher()
-		cacheItems, err := memcacher.MGet(keys)
-		if err != nil {
-			ctx.Log(err.Error())
-		}
+	// 	// 1. Find from memory first, if found, then return
+	// 	keys := []string{query1CacheKey, query2CacheKey}
+	// 	memcacher := ctx.MemCacher()
+	// 	cacheItems, err := memcacher.MGet(keys)
+	// 	if err != nil {
+	// 		ctx.Log(err.Error())
+	// 	}
 
-		members, _ = cacheItems[0].([]*Member)
-		counter, _ = cacheItems[1].(int)
-		if members != nil && counter > -1 {
-			resp := map[string]interface{}{
-				"status": "ok",
-				"total":  counter,
-				"items":  members,
-			}
-			ctx.Response(http.StatusOK, resp)
-			return nil
-		}
+	// 	members, _ = cacheItems[0].([]*Member)
+	// 	counter, _ = cacheItems[1].(int)
+	// 	if members != nil && counter > -1 {
+	// 		resp := map[string]interface{}{
+	// 			"status": "ok",
+	// 			"total":  counter,
+	// 			"items":  members,
+	// 		}
+	// 		ctx.Response(http.StatusOK, resp)
+	// 		return nil
+	// 	}
 
-		// 2. Find from redis
-		cacher := ctx.Cacher(NewCacherConfig())
-		cacheItems, err = cacher.MGet(keys)
-		if err != nil {
-			ctx.Log(err.Error())
-		}
+	// 	// 2. Find from redis
+	// 	cacher := ctx.Cacher(NewCacherConfig())
+	// 	cacheItems, err = cacher.MGet(keys)
+	// 	if err != nil {
+	// 		ctx.Log(err.Error())
+	// 	}
 
-		// The len of cacheItems will equal to the keys we send when call MGet
-		membersJS := cacheItems[0]
-		counterJS := cacheItems[1]
+	// 	// The len of cacheItems will equal to the keys we send when call MGet
+	// 	membersJS := cacheItems[0]
+	// 	counterJS := cacheItems[1]
 
-		// Found query #1 cache
-		if membersJS != nil && len(membersJS.(string)) > 0 {
-			// ctx.Log("cache hit")
-			err := json.Unmarshal([]byte(membersJS.(string)), &members)
-			if err != nil {
-				cacher.Del(query1CacheKey)
-				ctx.Log(err.Error())
-			}
-		}
+	// 	// Found query #1 cache
+	// 	if membersJS != nil && len(membersJS.(string)) > 0 {
+	// 		// ctx.Log("cache hit")
+	// 		err := json.Unmarshal([]byte(membersJS.(string)), &members)
+	// 		if err != nil {
+	// 			cacher.Del(query1CacheKey)
+	// 			ctx.Log(err.Error())
+	// 		}
+	// 	}
 
-		itemToCaches := map[string]interface{}{}
+	// 	itemToCaches := map[string]interface{}{}
 
-		if membersJS == nil {
-			// ctx.Log("cache miss")
-			members, err = queryLastestMembersFromDatabase(ctx, cfg)
-			if err != nil {
-				ctx.Response(http.StatusInternalServerError, map[string]interface{}{"status": "error"})
-				return nil
-			}
+	// 	if membersJS == nil {
+	// 		// ctx.Log("cache miss")
+	// 		members, err = queryLastestMembersFromDatabase(ctx, cfg)
+	// 		if err != nil {
+	// 			ctx.Response(http.StatusInternalServerError, map[string]interface{}{"status": "error"})
+	// 			return nil
+	// 		}
 
-			itemToCaches[query1CacheKey] = members
-		}
+	// 		itemToCaches[query1CacheKey] = members
+	// 	}
 
-		// Found query #2 cache
-		if counterJS != nil && len(counterJS.(string)) > 0 {
-			// ctx.Log("cache hit")
-			counter, err = strconv.Atoi(counterJS.(string))
-			if err != nil {
-				counter = -1
-				cacher.Del(query2CacheKey)
-				ctx.Log(err.Error())
-			}
-		}
+	// 	// Found query #2 cache
+	// 	if counterJS != nil && len(counterJS.(string)) > 0 {
+	// 		// ctx.Log("cache hit")
+	// 		counter, err = strconv.Atoi(counterJS.(string))
+	// 		if err != nil {
+	// 			counter = -1
+	// 			cacher.Del(query2CacheKey)
+	// 			ctx.Log(err.Error())
+	// 		}
+	// 	}
 
-		if counter < 0 {
-			// ctx.Log("cache miss")
-			counter, err = queryCountAllMembersFromDatabase(ctx, cfg)
-			if err != nil {
-				ctx.Response(http.StatusInternalServerError, map[string]interface{}{"status": "error"})
-				return nil
-			}
+	// 	if counter < 0 {
+	// 		// ctx.Log("cache miss")
+	// 		counter, err = queryCountAllMembersFromDatabase(ctx, cfg)
+	// 		if err != nil {
+	// 			ctx.Response(http.StatusInternalServerError, map[string]interface{}{"status": "error"})
+	// 			return nil
+	// 		}
 
-			itemToCaches[query2CacheKey] = fmt.Sprintf("%d", counter)
-		}
+	// 		itemToCaches[query2CacheKey] = fmt.Sprintf("%d", counter)
+	// 	}
 
-		if len(itemToCaches) > 0 {
+	// 	if len(itemToCaches) > 0 {
 
-			timeToExpire := 60 * 5 * time.Second // 5m
+	// 		timeToExpire := 60 * 5 * time.Second // 5m
 
-			// Set cache using MSET
-			err = cacher.MSet(itemToCaches)
-			if err != nil {
-				ctx.Log(err.Error())
-			}
+	// 		// Set cache using MSET
+	// 		err = cacher.MSet(itemToCaches)
+	// 		if err != nil {
+	// 			ctx.Log(err.Error())
+	// 		}
 
-			// Set time to expire
-			keys := []string{}
-			for k := range itemToCaches {
-				keys = append(keys, k)
-			}
-			err = cacher.Expires(keys, timeToExpire)
-			if err != nil {
-				ctx.Log(err.Error())
-			}
+	// 		// Set time to expire
+	// 		keys := []string{}
+	// 		for k := range itemToCaches {
+	// 			keys = append(keys, k)
+	// 		}
+	// 		err = cacher.Expires(keys, timeToExpire)
+	// 		if err != nil {
+	// 			ctx.Log(err.Error())
+	// 		}
 
-			// Set into local memory cache
-			err = memcacher.MSet(itemToCaches, timeToExpire)
-			if err != nil {
-				ctx.Log(err.Error())
-			}
-		}
+	// 		// Set into local memory cache
+	// 		err = memcacher.MSet(itemToCaches, timeToExpire)
+	// 		if err != nil {
+	// 			ctx.Log(err.Error())
+	// 		}
+	// 	}
 
-		resp := map[string]interface{}{
-			"status": "ok",
-			"total":  counter,
-			"items":  members,
-		}
-		ctx.Response(http.StatusOK, resp)
-		return nil
-	})
+	// 	resp := map[string]interface{}{
+	// 		"status": "ok",
+	// 		"total":  counter,
+	// 		"items":  members,
+	// 	}
+	// 	ctx.Response(http.StatusOK, resp)
+	// 	return nil
+	// })
 
 	// 5. Cleanup when exit
 	defer ms.Cleanup()
